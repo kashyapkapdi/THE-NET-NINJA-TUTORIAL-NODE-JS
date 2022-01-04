ba1213a8281c834323b949f413ad8edd691e0d03
kashyapkapdi <kashyapkapdi@rapidops.com>
2022-01-04 12:03:57 +0530

    #7 - Module Patterns (Third Example)

--- stuff.js
+++ stuff.js
@@ -1,9 +1,15 @@
-module.exports.counter = (count) => {
+let counter = (count) => {
   console.log(`There are ${count.length} elements in this array`);
 };
 
-module.exports.addition = (a, b) => {
+let addition = (a, b) => {
   console.log(`The sum of two number is: ${a + b}`);
 };
 
-module.exports.pi = 3.142;
+let pi = 3.142;
+
+module.exports = {
+  counter,
+  addition,
+  pi,
+};
