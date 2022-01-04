10b0011dfede4eee93c3a1269f0bb1af1a2d347a
kashyapkapdi <kashyapkapdi@rapidops.com>
2022-01-04 12:00:13 +0530

    #7 - Module Patterns(Second Example)

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ let stuff = require("./stuff");
 
 stuff.counter(["Kashyap", "Krupa", "Janvi"]);
 stuff.addition(3, 4);
+console.log(stuff.pi);
--- stuff.js
+++ stuff.js
@@ -1,13 +1,9 @@
-let counter = (count) => {
+module.exports.counter = (count) => {
   console.log(`There are ${count.length} elements in this array`);
 };
 
-let addition = (a, b) => {
+module.exports.addition = (a, b) => {
   console.log(`The sum of two number is: ${a + b}`);
 };
 
-let pi = 3.142;
-
-module.exports.counter = counter;
-module.exports.addition = addition;
-module.exports.pi = pi;
+module.exports.pi = 3.142;
