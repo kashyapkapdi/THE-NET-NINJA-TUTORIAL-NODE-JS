729128a79a6d9fa3990ad0589ad7840a922e0ccf
kashyapkapdi <kashyapkapdi@rapidops.com>
2022-01-05 13:12:29 +0530

    #25 - Template Engines(learn tamplateing engines how to work in node js)

---
+++ #25 - Template Engines/app.js
@@ -0,0 +1,25 @@
+let express = require("express");
+
+let app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/data", (req, res) => {
+  res.sendFile(__dirname + "/public/data.html");
+});
+
+app.get("/profile/:name", (req, res) => {
+  let data = {
+    age: 19,
+    job: "Jr software Engineer",
+  };
+  res.render("profile", { person: req.params.name, data: data });
+});
+
+app.listen(3000, () => {
+  console.log("The server run on port 3000");
+});
